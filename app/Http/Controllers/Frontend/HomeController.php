6df4884f4b7149ecbea7e6ac75450ae999a7ccef
hjgdinh <hjgdinh@gmail.com>
2025-05-19 10:00:19 +0700

    add limit for silder in home

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -12,6 +12,7 @@ class HomeController extends Controller
     {
         $employers = Employer::orderByRaw("CASE WHEN is_hot = 'yes' THEN 0 ELSE 1 END")
             ->orderBy('created_at', 'desc')
+            ->take(30)
             ->get();
 
         return view('Frontend.home', compact('employers'));
