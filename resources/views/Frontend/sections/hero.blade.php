51ae3bb2f713b944b0200dfeaf000c438b6d66d9
huutran96hy <80299380+huutran96hy@users.noreply.github.com>
2025-07-12 11:07:02 +0700

    11

--- resources/views/Frontend/sections/hero.blade.php
+++ resources/views/Frontend/sections/hero.blade.php
@@ -1,4 +1,4 @@
-<section class="relative h-[400px] lg:h-[480px] overflow-hidden hero-bg">
+<section class="relative h-[400px] lg:h-[470px] overflow-hidden hero-bg">
     <!-- Blue diagonal overlay -->
     
 </section>
