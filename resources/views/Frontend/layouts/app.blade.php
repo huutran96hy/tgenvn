3891e5ee0628772316e054f61c05ebab8cc7e8f0
huutran96hy <80299380+huutran96hy@users.noreply.github.com>
2025-07-14 22:56:13 +0700

    11

--- resources/views/Frontend/layouts/app.blade.php
+++ resources/views/Frontend/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'TG ENC - Precision Granite Stage Technology')</title>
+    <link rel="icon" href="{{ asset('assets/images/favicon.jpg') }}" type="image/x-icon">
     <script src="https://cdn.tailwindcss.com"></script>
     @include('Frontend.partials.styles')
 </head>
