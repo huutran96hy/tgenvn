739f86b93d40e21c5c77e63e3c0818e6f5a47983
huutran96hy <80299380+huutran96hy@users.noreply.github.com>
2025-07-20 23:54:32 +0700

    11

--- config/menu.php
+++ config/menu.php
@@ -9,24 +9,6 @@ return [
         ],
         [
             'route' => '#',
-            'label' => 'Quản lý công việc',
-            'icon' => 'ph-briefcase',
-            'children' => [
-                ['route' => 'admin.jobs.index', 'label' => 'Danh sách công việc'],
-                ['route' => 'admin.job-categories.index', 'label' => 'Danh mục công việc'],
-            ]
-        ],
-        [
-            'route' => '#',
-            'label' => 'Tin tức',
-            'icon' => 'ph-newspaper',
-            'children' => [
-                ['route' => 'admin.news.index', 'label' => 'Danh sách tin tức'],
-                ['route' => 'admin.news-categories.index', 'label' => 'Danh mục tin tức'],
-            ]
-        ],
-        [
-            'route' => '#',
             'label' => 'Sản phẩm',
             'icon' => 'ph-shopping-cart',
             'children' => [
@@ -42,36 +24,6 @@ return [
                 ['route' => 'admin.processes.index', 'label' => 'Danh sách quy trình'],
                 ['route' => 'admin.process-categories.index', 'label' => 'Danh mục quy trình'],
             ]
-        ],
-        [
-            'route' => 'admin.employers.index',
-            'label' => 'Danh sách nhà tuyển dụng',
-            'icon' => 'ph-briefcase',
-        ],
-        [
-            'route' => 'admin.candidates.index',
-            'label' => 'Danh sách ứng viên',
-            'icon' => 'ph-briefcase',
-        ],
-        [
-            'route' => 'admin.applications.index',
-            'label' => 'Danh sách ứng tuyển',
-            'icon' => 'ph-briefcase',
-        ],
-        [
-            'route' => 'admin.skills.index',
-            'label' => 'Danh sách kỹ năng',
-            'icon' => 'ph-briefcase',
-        ],
-        [
-            'route' => 'admin.contacts.index',
-            'label' => 'Danh sách liên hệ',
-            'icon' => 'ph-briefcase',
-        ],
-        [
-            'route' => 'admin.company-position.index',
-            'label' => 'Danh sách vị trí chức vụ',
-            'icon' => 'ph-briefcase',
         ],
         [
             'route' => 'admin.configs.index',
