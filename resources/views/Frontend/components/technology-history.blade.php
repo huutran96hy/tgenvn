7d8c46202a250e6cd77bebf15c3f8db75b0bf7ef
huutran96hy <80299380+huutran96hy@users.noreply.github.com>
2025-08-03 22:52:16 +0700

    11111

--- resources/views/Frontend/components/technology-history.blade.php
+++ resources/views/Frontend/components/technology-history.blade.php
@@ -1,6 +1,6 @@
 <div class="bg-white rounded-lg shadow-sm p-8 pt-0">
     <!-- History Table -->
     <div class="overflow-x-auto">
-        <img src="{{ asset('assets/images/technology-history.jpg') }}" alt="Technology History" class="w-full h-auto">
+        <img src="{{ asset('assets/images/technology-history-1.png') }}" alt="Technology History" class="w-full h-auto">
     </div>
 </div>
